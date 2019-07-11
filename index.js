41b32d0c6324bcbb0f35c398ce29b666dbf7440a
Honchar Denys <crackeraki@gmail.com>
2019-07-11 14:39:38 +0300

    config updated

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ module.exports = function(sd){
 		if(sd.config.mongo.user&&sd.config.mongo.pass){
 			mongoAuth = sd.config.mongo.user + ':' + sd.config.mongo.pass + '@';
 		}
-		sd._mongoUrl = 'mongodb://'+mongoAuth+(sd.config.mongo.host||'localhost')+':'+(sd.config.mongo.port||'27017')+'/'+(sd.config.mongo.db||'test');
+		sd.mongoUrl = 'mongodb://'+mongoAuth+(sd.config.mongo.host||'localhost')+':'+(sd.config.mongo.port||'27017')+'/'+(sd.config.mongo.db||'test');
+		if(mongoose.connection.readyState==0){
+			mongoose.connect(sd.mongoUrl, {
+				useNewUrlParser: true
+			});
+			mongoose.set('useCreateIndex', true);
+			mongoose.Promise = global.Promise;
+		}
 	}
 }
